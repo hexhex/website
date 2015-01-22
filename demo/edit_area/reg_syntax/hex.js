dd06fef86ed26899d3ed2c74adaa20a3b07b9e13
Cristoph Redl <redl@mahler.kr.tuwien.ac.at>
2015-01-22 14:42:51 +0100

    update hex syntax

--- demo/edit_area/reg_syntax/hex.js
+++ demo/edit_area/reg_syntax/hex.js
@@ -10,49 +10,43 @@ editAreaLoader.load_syntax["hex"] = {
 	,'COMMENT_MULTI' : {}
 	,'QUOTEMARKS' : {1: "'", 2: '"'}
 	,'KEYWORD_CASE_SENSITIVE' : true
-	,'KEYWORDS' : {
-		/*
-		** Set 1: reserved words
-		*/
-		'reserved' : [
-			'v'
-		]
-		/*
-		** Set 2: builtins
-		*/	
-		,'builtins' : [
-			'#int', '#min', '#max', '#avg', '#count'
-		]
-		/*
-		** Set 3: standard library
-		*/
-		,'stdlib' : [
-		]
-		/*
-		** Set 4: special methods
-		*/
-		,'special' : [
-			// Basic customization:
-			'&'
-		]
-	}
 	,'OPERATORS' :[
-		'+', '-', '/', '*', '=', '!=', '<', '>', '<=', '>='
+		'v', ':-', '.'
 	]
 	,'DELIMITERS' :[
-		'(', ')', '[', ']', '{', '}'
+		'(', ')', '[', ']', '{', '}', ','
 	]
+	,'REGEXPS' : {
+		'extat' : {
+			'search' : '()(\&[a-z,A-Z][a-z,A-Z,0-9]*)()'
+			,'class' : 'extat'
+			,'modifiers' : 'g'
+			,'execute' : 'before'
+		}
+                ,'aggregate' : {
+                        'search' : '()(\#maxint=[0-9]*|\#int|\#sum|\#min|\#max|\#avg|\#count)()'
+                        ,'class' : 'aggregate'
+                        ,'modifiers' : 'g'
+                        ,'execute' : 'before'
+                }
+		,'arithmetics' : {
+			'search' : '()(\\+|\\-|\\*|\\/|\\<|\\<\\=|\\>|\\>\\=|\\=)()'
+                        ,'class' : 'arithmetics'
+                        ,'modifiers' : 'g'
+                        ,'execute' : 'after'
+		}
+	}
 	,'STYLES' : {
 		'COMMENTS': 'color: #AAAAAA;'
-		,'QUOTESMARKS': 'color: #660066;'
-		,'KEYWORDS' : {
-			'reserved' : 'color: #0000FF;'
-			,'builtins' : 'color: #009900;'
-			,'stdlib' : 'color: #009900;'
-			,'special': 'color: #006666;'
+		,'QUOTESMARKS': 'color: #990099;'
+		,'OPERATORS' : 'color: #00AF00;'
+		,'DELIMITERS' : 'color: #0000FF;'
+		,'REGEXPS' : {
+			'extat' : 'color: #FF0000;'
+			,'aggregate' : 'color: #FF0099'
+			,'arithmetics' : 'color: #FF8000'
 			}
-		,'OPERATORS' : 'color: #993300;'
-		,'DELIMITERS' : 'color: #993300;'
+		,'EXT' : 'color: #AF0000;'
 				
 	}
 };
