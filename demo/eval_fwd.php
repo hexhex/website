3b02806b7642b16b530454470935945a87a6f888
Cristoph Redl <redl@mahler.kr.tuwien.ac.at>
2015-01-31 13:54:08 +0100

    fix a bug in demo

--- demo/eval_fwd.php
+++ demo/eval_fwd.php
@@ -1,5 +1,5 @@
 <?php
         header("Content-Type: text/plain");
 	# insert the IP of the server which actually evaluates the HEX-program; this server must also run a webserver with php support
-	print file_get_contents("http://128.130.205.74/demo/eval.php?mode=" . urlencode($_GET["mode"]) . "&commandlineoptions=" . $_GET["commandlineoptions"] . "&hexprogram=" . urlencode($_GET["hexprogram"]) . "&extsource=" . urlencode($_GET["extsource"]));
+	print file_get_contents("http://128.130.205.74/demo/eval.php?mode=" . urlencode($_GET["mode"]) . "&commandlineoptions=" . urlencode($_GET["commandlineoptions"]) . "&hexprogram=" . urlencode($_GET["hexprogram"]) . "&extsource=" . urlencode($_GET["extsource"]));
 ?>
