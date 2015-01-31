57caa946441cb8705ca1ea97761484edaf729f96
Cristoph Redl <redl@mahler.kr.tuwien.ac.at>
2015-01-31 15:06:40 +0100

    add example

---
+++ demo/examples/005_setpartitioning/plugin.py
@@ -0,0 +1,23 @@
+import dlvhex
+
+# computes the set of all elements in the extension
+# of unary predicate p but not in that of q
+def setdiff(p,q):
+	# for all input atoms (over p or q)
+	for x in dlvhex.getTrueInputAtoms():
+		# check if the predicate is p
+		if x.tuple()[0] == p:
+			# check if the atom with predicate
+			# being changed to q is NOT in the input
+			if dlvhex.isFalse(dlvhex.storeAtom((q, x.tuple()[1]))):
+				# then the element is in the output
+				dlvhex.output((x.tuple()[1], ));
+
+# register all external atoms
+def register():
+	# output element 0 can only contain values which appear in the input
+	prop = ExtSourceProperties()
+	prop.addRelativeFiniteOutputDomain(0, 0)
+        prop.addRelativeFiniteOutputDomain(0, 1)
+	# setdiff has two predicate input parametrers and its output arity is 1
+	dlvhex.addAtom("setdiff", (dlvhex.PREDICATE, dlvhex.PREDICATE), 1, prop)
